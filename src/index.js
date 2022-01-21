e312fc08b6759c1bc55900f96a68baa24b939cda
magudev <magubendev@gmail.com>
2022-01-21 17:15:23 -0300

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import {Todo, TodoList} from './classes';     // Busca el index.js por defecto
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+console.log('todos', todoList.todos);
